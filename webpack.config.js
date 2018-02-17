4f14a8839aee09b88f782893762ef5152c4d4bbc
Steve Wichelecki <swichelecki@gmail.com>
2018-02-17 14:34:04 -0600

    seventh commit

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['es2015', 'react', 'stage-0']
+                    presets: ['babel-preset-es2015', 'react', 'stage-0']
                 }
             },
             {
