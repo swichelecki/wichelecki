06497c40fd5077e9fd292d54b30a6488860864f3
Steve Wichelecki <swichelecki@gmail.com>
2018-02-19 21:25:55 -0600

    2-18-19-1

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,8 @@ module.exports = {
                 }
             },
             {
-               test: /\.css$/,
-               use: ['style-loader', 'css-loader']
+              /* test: /\.css$/,
+               use: ['style-loader', 'css-loader']*/
              }
           ]
         },
