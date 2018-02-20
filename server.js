1ba80c6e17caba2c75acad442e9f9ba7ce38ce15
Steve Wichelecki <swichelecki@gmail.com>
2018-02-19 21:12:16 -0600

    2-18-19 final commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/src/template/index.html'))
+  res.sendFile(path.resolve(__dirname, '/dist/index.html'))
 });
 
 app.listen(port);
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@ var path = require('path');
 
 var webpack = require('webpack');
 
-//var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-
 module.exports = {
     /*  entry: './src/index.js',
       output: { path: __dirname, filename: 'bundle.js'},*/
@@ -13,12 +10,12 @@ module.exports = {
         admin: path.join(__dirname, './src/index_admin.js')
     },
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: './[name].bundle.js',
     },
     watch: true,
     devServer: {
-        contentBase: __dirname + "/src/template/",
+        contentBase: __dirname + "/dist/",
         inline: true,
         host: '0.0.0.0',
         port: 8080,
@@ -35,9 +32,7 @@ module.exports = {
             },
             {
                test: /\.css$/,
-               use: ['style-loader', 'css-loader?minimize!'],
-               include: path.join(__dirname, 'src')
-               //use: ExtractTextPlugin.extract(['style-loader', 'css-loader'])
+               use: ['style-loader', 'css-loader']
              }
           ]
         },
@@ -47,12 +42,5 @@ module.exports = {
             normalize: path.join(__dirname, '/node_modules/normalize.css')
           }
         },
-      /*  plugins: [
-          new ExtractTextPlugin({
-            path: path.join(__dirname, 'dist'),
-            filename: './[name].min.css',
-            disabled: false,
-            allChunks: true
-          })
-        ]*/
+
 };
