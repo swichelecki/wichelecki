a84460aef3114500cf39c6fee902b5a74adb4428
Steve Wichelecki <swichelecki@gmail.com>
2018-02-19 21:28:19 -0600

    2-18-19-2

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,11 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'react', 'stage-0']
                 }
-            },
-            {
-              /* test: /\.css$/,
-               use: ['style-loader', 'css-loader']*/
-             }
+            }
+          /*  {
+               test: /\.css$/,
+               use: ['style-loader', 'css-loader']
+             }*/
           ]
         },
         resolve: {
