f794a745baa1696cb44f44c69bf2093c2f52be14
Steve Wichelecki <swichelecki@gmail.com>
2018-02-27 14:31:18 -0600

    2-27-18-1

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 var path = require('path');
 var webpack = require('webpack');
 
+/*var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css',
+});
+
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var CleanWebpackPlugin = require('clean-webpack-plugin');*/
 
 module.exports = {
     entry: {
@@ -11,7 +19,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
-        //publicPath: '/dist'
+        publicPath: '/dist'
     },
     watch: true,
     devServer: {
@@ -37,6 +45,9 @@ module.exports = {
             },
             {
                test: /\.css$/,
+              /* use: extractPlugin.extract({
+                 use: ['css-loader']
+               }), */
                use: ['style-loader', 'css-loader']
              },
              {
@@ -50,7 +61,7 @@ module.exports = {
                    loader: 'file-loader',
                    options: {
                      name: '[name].[ext]',
-                     outputPath: 'images/',
+                     outputPath: 'src/images/',
                      publicPath: 'images/'
                   }
                 }
@@ -58,20 +69,22 @@ module.exports = {
             }
           ]
       },
-        resolve: {
+      resolve: {
           extensions: ['.js', '.jsx'],
           alias: {
             normalize: path.join(__dirname, '/node_modules/normalize.css')
           }
-        },
-        plugins: [
-          new HtmlWebpackPlugin({
+      },
+      plugins: [
+        /*  new HtmlWebpackPlugin({
               filename: 'index.html',
               template: 'src/index.html'
           }),
           new HtmlWebpackPlugin({
               filename: 'admin.html',
               template: 'src/admin.html'
-          })
-        ]
+          }),
+          new CleanWebpackPlugin(['dist']),
+           extractPlugin*/
+      ]
 };
