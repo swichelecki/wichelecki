24c2d7193b0de2edbf9dc8560b816b8e430a6eb8
Steve Wichelecki <swichelecki@gmail.com>
2018-02-28 08:51:00 -0600

    2-28-18-3

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
-        publicPath: '/dist'
+        //publicPath: '/dist'
     },
     watch: true,
     devServer: {
