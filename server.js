944c8e59d4d138b8018cb65ec0b12b827ae67d6e
Steve Wichelecki <swichelecki@wciu.com>
2018-12-01 18:21:32 -0600

    refactoring commit 2

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 });
 
 app.listen(port);
-console.log('success');
